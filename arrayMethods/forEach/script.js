7fad69cd589c06813e7690f953e9db3b2c63ade0
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-05-06 12:49:44 -0300

    array methods

---
+++ arrayMethods/forEach/script.js
@@ -0,0 +1,9 @@
+var numeros = [0, 1, 2, 3, 4, 5];
+
+numeros.forEach((numero)=>{
+    if( numero % 2 === 0) {
+        console.log(numero + ' é par')
+    } else {
+        console.log(numero + ' é impar')
+    }
+})
